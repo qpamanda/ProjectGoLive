d5b4bc4664e5eb80067f1a48d9e322ecf84aa561
CMping <CMping>
2021-06-27 12:52:07 +0800

    added logging , validateContact feature, onclick function in html

--- ProjectGoLive/server/recipientHandler.go
+++ ProjectGoLive/server/recipientHandler.go
@@ -7,12 +7,29 @@ package server
 import (
 	"ProjectGoLive/authenticate"
 	"ProjectGoLive/database"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"sort"
 	"strconv"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
+
+type Result struct {
+	Valid                bool
+	Number               string
+	Local_format         string
+	International_format string
+	Country_prefix       string
+	Country_code         string
+	Country_name         string
+	Location             string
+	Carrier              string
+	Line_type            string
+}
 
 type Recipient struct {
 	RecipientID    int
@@ -42,6 +59,7 @@ func manageRecipient(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		clientMsg = "Internal server error at database"
+		log.Error(err)
 	}
 
 	// check if there is any recipient in the database under this RepID
@@ -89,6 +107,7 @@ func addRecipient(w http.ResponseWriter, r *http.Request) {
 		// check if there is any empty field
 		if name == "" || profile == "" || contactNo == "" {
 			clientMsg = "Field cannot empty"
+			log.Error("Empty Field in creating recipient")
 			data := struct {
 				User      authenticate.User
 				IsAdmin   bool
@@ -102,19 +121,58 @@ func addRecipient(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var categoryBool bool
-
-		if category == "Individual" {
-			categoryBool = true
-		}
-
-		err := database.AddRecipient(myUser.RepID, name, categoryBool, profile, contactNo)
+		validContact, err := validateContactNo(contactNo)
 
 		if err != nil {
-			fmt.Println(err)
-			clientMsg = "Internal server error at database"
+			log.Error(err)
+			log.Error("Problem with numverify API")
+			clientMsg = "Server error"
+			data := struct {
+				User      authenticate.User
+				IsAdmin   bool
+				ClientMsg string
+			}{
+				myUser,
+				authenticate.IsAdmin,
+				clientMsg,
+			}
+			tpl.ExecuteTemplate(w, "addRecipient.gohtml", data)
+			return
+		}
+
+		if !validContact {
+			log.Error("Invalid contact number at creating recipient")
+			clientMsg = "Invalid contact number, please only enter the 8-digit numbers"
+			data := struct {
+				User      authenticate.User
+				IsAdmin   bool
+				ClientMsg string
+			}{
+				myUser,
+				authenticate.IsAdmin,
+				clientMsg,
+			}
+			tpl.ExecuteTemplate(w, "addRecipient.gohtml", data)
+			return
 		} else {
-			clientMsg = "You have successfully created a new recipient"
+
+			var categoryBool bool
+
+			if category == "Individual" {
+				categoryBool = true
+			}
+
+			err := database.AddRecipient(myUser.RepID, name, categoryBool, profile, contactNo)
+
+			if err != nil {
+				log.Error(err)
+				clientMsg = "Internal server error at database"
+			} else {
+				log.WithFields(logrus.Fields{
+					"userName": myUser.UserName,
+				}).Infof("[%s] created a new recipient: [%s]", myUser.UserName, name)
+				clientMsg = "You have successfully created a new recipient"
+			}
 		}
 	}
 	data := struct {
@@ -149,6 +207,7 @@ func getRecipient(w http.ResponseWriter, r *http.Request) {
 
 	// checking for error when converting the string to int64
 	if err != nil {
+		log.Error(err)
 		clientMsg = "Internal server error"
 	}
 
@@ -157,6 +216,7 @@ func getRecipient(w http.ResponseWriter, r *http.Request) {
 
 	// checking for error at the database query
 	if err != nil {
+		log.Error(err)
 		clientMsg = "Internal server error at database"
 	}
 
@@ -195,7 +255,8 @@ func updateRecipient(w http.ResponseWriter, r *http.Request) {
 
 	// checking for error when converting the string to int64
 	if err != nil {
-		clientMsg = "Internal server error"
+		log.Error(err)
+		clientMsg = "Internal server error, update unsuccessful"
 	}
 
 	if r.Method == http.MethodPost {
@@ -207,6 +268,7 @@ func updateRecipient(w http.ResponseWriter, r *http.Request) {
 
 		// check if there is any empty field
 		if name == "" || profile == "" || contactNo == "" {
+			log.Error("Empty Field in updating recipient")
 			clientMsg = "Field cannot empty"
 			recipient, _ := database.GetRecipient(myUser.RepID, recipientIDInt)
 			data := struct {
@@ -224,22 +286,13 @@ func updateRecipient(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var categoryBool bool
-
-		if category == "Individual" {
-			categoryBool = true
-		}
-
-		err := database.UpdateRecipient(myUser.RepID, recipientIDInt, name, categoryBool, profile, contactNo)
+		validContact, err := validateContactNo(contactNo)
 
 		if err != nil {
-			clientMsg = "Internal server error at database"
-		} else {
-			clientMsg = "You have successfully updated " + name + "."
-			recipient, err := database.GetRecipient(myUser.RepID, recipientIDInt)
-			if err != nil {
-				clientMsg = "Internal server error at database"
-			}
+			log.Error(err)
+			log.Error("Problem with numverify API")
+			clientMsg = "Server error"
+			recipient, _ := database.GetRecipient(myUser.RepID, recipientIDInt)
 			data := struct {
 				User      authenticate.User
 				IsAdmin   bool
@@ -251,8 +304,64 @@ func updateRecipient(w http.ResponseWriter, r *http.Request) {
 				recipient,
 				clientMsg,
 			}
-			tpl.ExecuteTemplate(w, "getRecipient.gohtml", data)
+			tpl.ExecuteTemplate(w, "updateRecipient.gohtml", data)
 			return
+		}
+
+		if !validContact {
+			log.Error("Invalid contact number at updating recipient")
+			clientMsg = "Invalid contact number, please only enter the 8-digit numbers"
+			recipient, _ := database.GetRecipient(myUser.RepID, recipientIDInt)
+			data := struct {
+				User      authenticate.User
+				IsAdmin   bool
+				Recipient database.Recipient
+				ClientMsg string
+			}{
+				myUser,
+				authenticate.IsAdmin,
+				recipient,
+				clientMsg,
+			}
+			tpl.ExecuteTemplate(w, "updateRecipient.gohtml", data)
+			return
+		} else {
+
+			var categoryBool bool
+
+			if category == "Individual" {
+				categoryBool = true
+			}
+
+			err = database.UpdateRecipient(myUser.RepID, recipientIDInt, name, categoryBool, profile, contactNo)
+
+			if err != nil {
+				log.Error(err)
+				clientMsg = "Internal server error at database, update unsuccessful"
+			} else {
+				log.WithFields(logrus.Fields{
+					"userName": myUser.UserName,
+				}).Infof("[%s] updated recipientID: [%s]", myUser.UserName, recipientIDInt)
+				clientMsg = "You have successfully updated " + name + "."
+				recipient, err := database.GetRecipient(myUser.RepID, recipientIDInt)
+				if err != nil {
+					log.Error(err)
+					clientMsg = "Internal server error at database"
+				}
+				data := struct {
+					User      authenticate.User
+					IsAdmin   bool
+					Recipient database.Recipient
+					ClientMsg string
+				}{
+					myUser,
+					authenticate.IsAdmin,
+					recipient,
+					clientMsg,
+				}
+				tpl.ExecuteTemplate(w, "getRecipient.gohtml", data)
+				return
+			}
 		}
 	}
 
@@ -261,6 +370,7 @@ func updateRecipient(w http.ResponseWriter, r *http.Request) {
 
 	// checking for error at the database query
 	if err != nil {
+		log.Error(err)
 		clientMsg = "Internal server error at database"
 	}
 
@@ -299,7 +409,8 @@ func deleteRecipient(w http.ResponseWriter, r *http.Request) {
 
 	// checking for error when converting the string to int64
 	if err != nil {
-		clientMsg2 = "Internal server error"
+		log.Error(err)
+		clientMsg2 = "Internal server error, delete unsuccessful."
 	}
 
 	// query the database and get the recipient information
@@ -307,13 +418,18 @@ func deleteRecipient(w http.ResponseWriter, r *http.Request) {
 
 	// checking for error at the database query
 	if err2 != nil {
-		clientMsg2 = "Internal server error at database"
+		log.Error(err2)
+		clientMsg2 = "Internal server error at database, delete unsuccessful."
 	} else {
-		clientMsg2 = "You have successfully deleted a recipient"
+		log.WithFields(logrus.Fields{
+			"userName": myUser.UserName,
+		}).Infof("[%s] deleted recipientID: [%s]", myUser.UserName, recipientIDInt)
+		clientMsg2 = "You have successfully deleted a recipient."
 	}
 	recipients, err3 := database.GetMyRecipient(myUser.RepID)
 
 	if err3 != nil {
+		log.Error(err3)
 		clientMsg2 = "Internal server error at database"
 	}
 
@@ -338,4 +454,26 @@ func deleteRecipient(w http.ResponseWriter, r *http.Request) {
 		clientMsg2,
 	}
 	tpl.ExecuteTemplate(w, "manageRecipient.gohtml", data)
+}
+
+func validateContactNo(contactNo string) (bool, error) {
+	accessKey := "ce2db76b60065070752e50aab06b23a5"
+	url := "http://apilayer.net/api/validate?access_key=" + accessKey + "&number=" + contactNo + "&country_code=SG&format=1"
+	if resp, err := http.Get(url); err == nil {
+		defer resp.Body.Close()
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			var result Result
+			json.Unmarshal(body, &result)
+			fmt.Println(result)
+			if result.Valid {
+				return true, nil
+			} else {
+				return false, nil
+			}
+		} else {
+			return false, err
+		}
+	} else {
+		return false, err
+	}
 }
